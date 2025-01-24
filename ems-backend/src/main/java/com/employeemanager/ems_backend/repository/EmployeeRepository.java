fc43ce01192bd8003951b436c434d258bc5b5152
JhonPoma <jpm11797@gmail.com>
2025-01-24 17:20:49 -0500

    creamos Employee JPA entity, EmployeeRepository

---
+++ ems-backend/src/main/java/com/employeemanager/ems_backend/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.employeemanager.ems_backend.repository;
+
+import com.employeemanager.ems_backend.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+
+}
